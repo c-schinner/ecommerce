dd0c56e99f7e70ba021b512504ca7fe810a7032d
Chris <cschinner7@gmail.com>
2024-10-08 15:10:59 -0700

    fixed errors

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -4,17 +4,17 @@ import { NextRequest, NextResponse } from "next/server";
 import fs from "fs/promises";
 
 export async function GET(req: NextRequest, { params: { id }}: { params: { id: string}}) {
-    const product = db.product.findUnique({ 
+    const product = await db.product.findUnique({ 
         where: { id}, 
         select: { filePath: true, name: true} })
 
     if (product == null) return notFound()
 
-    const { size } = await fs.stat(product.filePath!)
-    const file = await fs.readFile(product.filePath!)
-    const extension = product.filePath!.split(".").pop()
+    const { size } = await fs.stat(product.filePath)
+    const file = await fs.readFile(product.filePath)
+    const extension = product.filePath.split(".").pop()
 
     return new NextResponse(file, { headers: {
-        "Content-Disposition": `attachment; filename="${product.name!}.${extension}`
+        "Content-Disposition": `attachment; filename="${product.name}.${extension}`
     }})
 }
