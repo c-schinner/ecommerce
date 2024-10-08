b3c3103046be2d0b8bedd7657d8463d33fa716f7
Chris <cschinner7@gmail.com>
2024-10-08 10:10:33 -0700

    delete functionality of product

--- src/app/admin/products/_components/ProductActions.tsx
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -1,5 +1,9 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { useTransition } from "react";
+import {
+    deleteProduct,
+    toggleProductAvailability,
+} from "../../_actions/products";
 
 export function ActiveToggleDropdownItem({
     id,
@@ -11,6 +15,7 @@ export function ActiveToggleDropdownItem({
     const [isPending, startTransition] = useTransition();
     return (
         <DropdownMenuItem
+            disabled={isPending}
             onClick={() => {
                 startTransition(async () => {
                     await toggleProductAvailability(
@@ -19,8 +24,30 @@ export function ActiveToggleDropdownItem({
                     );
                 });
             }}
-        ></DropdownMenuItem>
+        >
+            {isAvailableForPurchase ? "Deactivate" : "Activate"}
+        </DropdownMenuItem>
     );
 }
 
-export function DeactivateDropdownItem() {}
+export function DeactivateDropdownItem({
+    id,
+    disabled,
+}: {
+    id: string;
+    disabled: boolean;
+}) {
+    const [isPending, startTransition] = useTransition();
+    return (
+        <DropdownMenuItem
+            disabled={disabled || isPending}
+            onClick={() => {
+                startTransition(async () => {
+                    await deleteProduct(id);
+                });
+            }}
+        >
+            Delete
+        </DropdownMenuItem>
+    );
+}
