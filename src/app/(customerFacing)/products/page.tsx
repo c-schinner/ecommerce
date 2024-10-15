5bac5c7295e6a3439aa5c05a510af52e6326559d
Chris <cschinner7@gmail.com>
2024-10-15 11:34:36 -0700

    adding cacheing to our functions

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,12 +1,16 @@
 import { ProductCard, ProductCardSkeleton } from "@/app/components/ProductCard";
 import db from "@/db/db";
+import { cache } from "@/lib/cache";
 import { Suspense } from "react";
 
-function getProducts() {
-    return db.product.findMany({ where: { isAvailableForPurchase: true } });
-}
+const getProducts = cache(() => {
+    return db.product.findMany({
+        where: { isAvailableForPurchase: true },
+        orderBy: { name: "asc" },
+    });
+}, ["/products", "getProducts"]);
 
-export function ProductsPage() {
+export default function ProductsPage() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <Suspense
