e3eee7aed0db8e2f73f0ceadc39557dd4a20f785
Chris <cschinner7@gmail.com>
2024-10-15 11:40:22 -0700

    adding purchase page

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -4,6 +4,7 @@ import db from "@/db/db";
 import { z } from "zod";
 import fs from "fs/promises"
 import { notFound, redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 const fileSchema = z.instanceof(File, {message: "Required"})
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
@@ -57,6 +58,8 @@ export async function updateProduct(id: string, prevState: unknown, formData: Fo
         imagePath,
     }})
 
+    revalidatePath("/")
+    revalidatePath("/products")
     redirect("/admin/products")
 }
 
@@ -87,11 +90,16 @@ export async function addProduct(prevState: unknown, formData: FormData) {
         imagePath,
     }})
 
+    revalidatePath("/")
+    revalidatePath("/products")
     redirect("/admin/products")
 }
 
 export async function toggleProductAvailability(id: string, isAvailableForPurchase: boolean) {
     await db.product.update({ where: { id }, data: { isAvailableForPurchase } })
+
+    revalidatePath("/")
+    revalidatePath("/products")
 }
 
 export async function deleteProduct(id: string) {
@@ -100,4 +108,7 @@ export async function deleteProduct(id: string) {
 
     await fs.unlink(product.filePath)
     await fs.unlink(`public${product.imagePath}`)
+
+    revalidatePath("/")
+    revalidatePath("/products")
 }
