15bc3994734efc9edeedca0cdbd1da30c50f6a9c
Chris <cschinner7@gmail.com>
2024-10-17 10:07:56 -0700

    if order exists function

--- src/app/(customerFacing)/products/[id]/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/_components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { userOrderExists } from "@/app/actions/orders";
 import { Button } from "@/app/components/ui/button";
 import {
     Card,
@@ -27,6 +28,7 @@ type CheckoutFormProps = {
         name: string;
         priceInCents: number;
         description: string;
+        id: string;
     };
     clientSecret: string;
 };
@@ -59,26 +61,46 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
                     </div>
                 </div>
                 <Elements options={{ clientSecret }} stripe={stripePromise}>
-                    <Form priceInCents={product.priceInCents} />
+                    <Form
+                        priceInCents={product.priceInCents}
+                        productId={product.id}
+                    />
                 </Elements>
             </div>
         </>
     );
 }
 
-function Form({ priceInCents }: { priceInCents: number }) {
+function Form({
+    priceInCents,
+    productId,
+}: {
+    priceInCents: number;
+    productId: string;
+}) {
     const stripe = useStripe();
     const elements = useElements();
     const [isLoading, setIsLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState<string>();
     const [email, setEmail] = useState<string>();
 
-    function handleSubmit(e: FormEvent) {
+    async function handleSubmit(e: FormEvent) {
         e.preventDefault();
 
-        if (stripe == null || elements == null) return;
+        if (stripe == null || elements == null || email == null) return;
 
         setIsLoading(true);
+
+        // check for existing order
+        const orderExists = await userOrderExists(email, productId);
+
+        if (orderExists) {
+            setErrorMessage(
+                "You have already purchased this product. Try downloading it from the My Orders Page."
+            );
+            setIsLoading(false);
+            return;
+        }
 
         stripe
             .confirmPayment({
