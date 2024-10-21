e60b200eae05c9b609a43711879864a286eb815b
Chris <cschinner7@gmail.com>
2024-10-21 12:16:31 -0700

    getting the functionality of the stripe webhook

--- src/app/webhooks/stripe/route.ts
+++ src/app/webhooks/stripe/route.ts
@@ -1,4 +1,11 @@
-// This is going to be stripe hook page
+import { NextRequest } from "next/server";
+import Stripe from "stripe"
 
 // Go to the stripe page and follow the create local listener directions to complete this.
 // Im going to skip for now, because we dont really need this functionality at the moment.
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+
+export async function POST(req: NextRequest) {
+    stripe.webhooks.constructEvent(await req.text(), req.headers.get("stripe-signature"), process.env.STRIPE_WEBHOOK_SECRET)
+}
