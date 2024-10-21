2609b282039c19f0977169b80ff02344d10df62a
Chris <cschinner7@gmail.com>
2024-10-21 12:05:34 -0700

    stripe local listener

--- src/app/webhooks/stripe/route.ts
+++ src/app/webhooks/stripe/route.ts
@@ -1,1 +1,4 @@
 // This is going to be stripe hook page
+
+// Go to the stripe page and follow the create local listener directions to complete this.
+// Im going to skip for now, because we dont really need this functionality at the moment.
