6d462037fac20daaab40e1e11b384e41b3fe3d1e
Chris <cschinner7@gmail.com>
2024-10-22 11:02:12 -0700

    order and purchase receipt

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -7,8 +7,31 @@ import {
     Preview,
     Tailwind,
 } from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
 
-export default function PurchaseReceiptEmail({ product }) {
+type PurchaseReceiptEmailProps = {
+    product: {
+        name: string;
+    };
+    order: { id: string; createdAt: Date; pricePaidInCents: number };
+    downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+    product: { name: "Product name" },
+    order: {
+        id: crypto.randomUUID(),
+        createdAt: new Date(),
+        pricePaidInCents: 1000,
+    },
+    downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+    product,
+    order,
+    downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
     return (
         <Html>
             <Preview>Download {product.name} and view receipt</Preview>
@@ -17,6 +40,11 @@ export default function PurchaseReceiptEmail({ product }) {
                 <Body className="font-sans bg-white">
                     <Container className="mac-w-xl">
                         <Heading>Purchase Receipt</Heading>
+                        <OrderInformation
+                            order={order}
+                            product={product}
+                            downloadVerificationId={downloadVerificationId}
+                        />
                     </Container>
                 </Body>
             </Tailwind>
