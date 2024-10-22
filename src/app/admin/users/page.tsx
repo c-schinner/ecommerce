8e55bce99c75b43735f215f3f9ae6ff9ea66690e
Chris <cschinner7@gmail.com>
2024-10-22 09:41:10 -0700

    creating user page

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,91 @@
+import { PageHeader } from "../_components/PageHeader";
+import Link from "next/link";
+import db from "@/db/db";
+import { MoreVertical } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/app/components/ui/button";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/app/components/ui/table";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/app/components/ui/dropdown-menu";
+import {
+    ActiveToggleDropdownItem,
+    DeleteDropdownItem,
+} from "../products/_components/ProductActions";
+
+function getUsers() {
+    return db.product.findMany({
+        select: {
+            id: true,
+            email: true,
+            orders: { select: { pricePaidInCents: true } },
+        },
+        orderBy: { createdAt: "desc" },
+    });
+}
+
+export default function UserPage() {
+    return (
+        <>
+            <PageHeader>Customers</PageHeader>
+            <UsersTable />
+        </>
+    );
+}
+
+async function UsersTable() {
+    const users = await getUsers();
+
+    if (users.length === 0) return <p>No customers found</p>;
+
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Orders</TableHead>
+                    <TableHead>Value</TableHead>
+                    <TableHead className="w-0">
+                        <span className="sr-only">Actions</span>
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {users.map((user) => {
+                    <TableRow key={user.id}>
+                        <TableCell>{user.email}</TableCell>
+                        <TableCell>
+                            {formatNumber(user.orders.length)}
+                        </TableCell>
+                        <TableCell>
+                            {formatCurrency(
+                                user.orders.reduce(
+                                    (sum, o) => o.pricePaidInCents + sum,
+                                    0
+                                ) / 100
+                            )}
+                        </TableCell>
+                        <TableCell className="text-center">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <MoreVertical />
+                                </DropdownMenuTrigger>
+                            </DropdownMenu>
+                        </TableCell>
+                    </TableRow>;
+                })}
+            </TableBody>
+        </Table>
+    );
+}
