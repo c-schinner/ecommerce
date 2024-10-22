63c639254d31ec20958122de43f3807752b83e2a
Chris <cschinner7@gmail.com>
2024-10-22 10:42:01 -0700

    building product receipt page

---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,25 @@
+import {
+    Body,
+    Container,
+    Head,
+    Heading,
+    Html,
+    Preview,
+    Tailwind,
+} from "@react-email/components";
+
+export default function PurchaseReceiptEmail({ product }) {
+    return (
+        <Html>
+            <Preview>Download {product.name} and view receipt</Preview>
+            <Tailwind>
+                <Head />
+                <Body className="font-sans bg-white">
+                    <Container className="mac-w-xl">
+                        <Heading>Purchase Receipt</Heading>
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    );
+}
