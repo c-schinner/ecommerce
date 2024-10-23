af24f325e7d362eb9bb52472169eb8af0d9eaeff
Chris <cschinner7@gmail.com>
2024-10-23 10:11:03 -0700

    getting our order history functionality

--- src/app/(customerFacing)/orders/page.tsx
+++ src/app/(customerFacing)/orders/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { Button } from "@/app/components/ui/button";
 import {
     Card,
+    CardContent,
     CardDescription,
+    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/app/components/ui/card";
+import { Input } from "@/app/components/ui/input";
+import { Label } from "@/app/components/ui/label";
+import { useFormStatus } from "react-dom";
 
 export default function MyOrdersPage() {
     return (
@@ -16,7 +24,26 @@ export default function MyOrdersPage() {
                         history and download links
                     </CardDescription>
                 </CardHeader>
+                <CardContent>
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input type="email" required name="email" id="email" />
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <SubmitButton />
+                </CardFooter>
             </Card>
         </form>
+    );
+}
+
+function SubmitButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <Button className="w-full" size="lg" disabled={pending} type="submit">
+            {pending ? "Sending..." : "Send"}
+        </Button>
     );
 }
