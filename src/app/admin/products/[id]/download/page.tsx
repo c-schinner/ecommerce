5d93bb3b177ae3aef045268f2aa25c0b851f33e0
Chris <cschinner7@gmail.com>
2025-05-20 16:58:10 -0700

    updates

---
+++ src/app/admin/products/[id]/download/page.tsx
@@ -0,0 +1,6 @@
+// page.tsx
+export const dynamic = "force-dynamic";
+
+export default function AdminDownloadPage() {
+    return null;
+}
--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -3,19 +3,27 @@ import { notFound } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 import fs from "fs/promises";
 
-export async function GET(req: NextRequest, { params: { id }}: { params: { id: string}}) {
-    const product = await db.product.findUnique({ 
-        where: { id}, 
-        select: { filePath: true, name: true} })
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+export async function GET(
+    req: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    const product = await db.product.findUnique({
+        where: { id },
+        select: { filePath: true, name: true },
+    });
 
-    if (product == null) return notFound()
+    if (product == null) return notFound();
 
-    const { size } = await fs.stat(product.filePath)
-    const file = await fs.readFile(product.filePath)
-    const extension = product.filePath.split(".").pop()
+    const { size } = await fs.stat(product.filePath);
+    const file = await fs.readFile(product.filePath);
+    const extension = product.filePath.split(".").pop();
 
-    return new NextResponse(file, { headers: {
-        "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
-        "Content-Lenght": size.toString(),
-    }})
+    return new NextResponse(file, {
+        headers: {
+            "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
+            "Content-Length": size.toString(),
+        },
+    });
 }
